e0288d80371a34db577a501ba62e90059f381def
tal7aouy <tal7aouy@gmail.com>
2022-04-02 13:09:01 +0000

    ✔ updated

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,4 +1,18 @@
-import IColors from './IColors'
+interface IColors {
+  chalky: string
+  coral: string
+  dark: string
+  error: string
+  fountainBlue: string
+  green: string
+  invalid: string
+  lightDark: string
+  lightWhite: string
+  malibu: string
+  purple: string
+  whiskey: string
+  deepRed: string
+}
 
 // colors used for our theme
 const colors: IColors = {
