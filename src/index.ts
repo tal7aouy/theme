e9e38a0c6085247d447b2b74ab51a29eb2f154a0
tal7aouy <tal7aouy@gmail.com>
2022-04-25 17:39:07 +0000

    add:Themeflat

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { ThemeItalics } from '~/variants/themeItalic'
 import { build } from '~/scripts/build'
 import { pressEnterToExit } from '~/utils/press-enter-to-exit'
 import Theme from '~/variants/theme'
+import ThemeFlat from '~/variants/ThemeFlat'
+import { ThemeItalics } from '~/variants/themeItalic'
 
 async function run() {
   await build(Theme)
   await build(ThemeItalics)
+  await build(ThemeFlat)
 
   if (process.env.DEBUG_VSCODE) {
     pressEnterToExit() // Hold the process alive
